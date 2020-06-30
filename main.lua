f207cefe690491584a7962e4e25bfabb63e6c11e
Brian Kozub <ticet11@gmail.com>
2020-06-30 15:05:53 -0600

    added push.lua and refactored main.lua to use it.

--- main.lua
+++ main.lua
@@ -11,20 +11,34 @@ VIRTUAL_HEIGHT = 243
 -- love.load runs once, when the game initializes
 
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
+function love.keypressed(key)
+    -- key accessed by string name
+    if key == 'escape' then
+        -- terminate application
+        love.event.quit()
+    end
+end
+
 -- love.draw is called after update by love2d, used to draw anything to the screen
 
 function love.draw()
+    -- render at virtual resolution
+    push:apply('start')
+
     love.graphics.printf('Hello Pong!', -- test to render
     0, -- Starting x, 0 because we'll center it
-    WINDOW_HEIGHT / 2 - 6, -- Starting Y, halfway down the screen
-    WINDOW_WIDTH, -- number of pixels to center within the entire screen
+    VIRTUAL_HEIGHT / 2 - 6, -- Starting Y, halfway down the screen
+    VIRTUAL_WIDTH, -- number of pixels to center within the entire screen
     'center' -- alignment mode, can be 'center, 'left', or 'right'
     )
+
+    -- end rendering with virtual resolution
+    push:apply('end')
 end
