c69d52570f616b4c58023b5037911a98a7adea8e
Brian Kozub <ticet11@gmail.com>
2020-07-01 13:53:39 -0600

    put ball functionality into ball class file.

---
+++ Ball.lua
@@ -0,0 +1,30 @@
+Ball = Class()
+
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    -- variables to keep track of velocity on x and y axis
+    self.dy = math.random(2) == 1 and -100 or 100
+    self.dx = math.random(-50, 50)
+end
+
+-- places ball in middle of the screen
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dy = math.random(2) == 1 and -100 or 100
+    self.dx = math.random(-50, 50)
+end
+
+-- applies velocity to postion, scaled by dt
+function Ball.update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
