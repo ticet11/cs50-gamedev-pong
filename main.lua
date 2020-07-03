0f3fdf2fb20b6e30ab1788506b1e60d1821fe4a9
Brian Kozub <ticet11@gmail.com>
2020-07-03 13:50:35 -0600

    upper and lower boundary collision added.

--- main.lua
+++ main.lua
@@ -67,6 +67,43 @@ function love.load()
 end
 
 function love.update(dt)
+    if gameState == 'play' then
+        -- ball collision with paddles -> increase speed in opposite direction
+        if ball:collides(player1) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player1.x + 5
+
+            -- randomize y velocity
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+        end
+        if ball:collides(player2) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player2.x - 4
+
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+        end
+
+        -- detect upper and lower boundary collision
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.dy = -ball.dy
+        end
+
+        -- account for ball's size
+        if ball.y >= VIRTUAL_HEIGHT - 4 then
+            ball.y = VIRTUAL_HEIGHT - 4
+            ball.dy = -ball.dy
+        end
+    end
+
     -- player 1 movement
     if love.keyboard.isDown('w') then
         -- add negative paddle speed to current y scaled by deltaTime
